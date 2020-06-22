ea2a3199b7c41137350f19cc516d2d2d8fe30cf4
sabrina-code <54184701+sabrina-code@users.noreply.github.com>
2020-06-21 23:36:53 -0700

    tidyup

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-// Dependencies
 const express = require("express");
 const bodyParser = require('body-parser');
 const path = require("path");
 const fs = require('fs');
 const db = require("./db/db.json");
 
-// Express App
 const app = express();
-const PORT = 3000;
 
-// Express data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public')); //to include css
@@ -22,7 +18,7 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-//DATA
+//DATA: read, write, delete
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
@@ -31,37 +27,36 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  // const db = require("./db/db.json");
   db.push(req.body); //index.js #53
 
   for (var i = 0; i < db.length; i++) {
-    db[i]["id"] = JSON.parse(i+1);
+    db[i]["id"] = JSON.parse(i + 1);
   };
   const notesPost = JSON.stringify(db, null, 2);
   fs.writeFile('./db/db.json', notesPost, (err, data) => {
     if (err) throw err;
-    // res.json(data);
     res.redirect("/notes"); //refresh the html
   });
 });
 
 app.delete('/api/notes/:id', function (req, res) {
-  var id = JSON.parse(req.params.id); //the id of the item to delete typeof : string // console.log(typeof id);
-    
+  var id = JSON.parse(req.params.id); //delete by id -- // console.log(typeof id);
+
   fs.readFile("./db/db.json", 'utf8', (err, data) => {
     data = JSON.parse(data);  //all items in the json object //console.log(data);
     let newData = data.filter(note => note.id !== id);
-    console.log('=======> ', newData);  
+    console.log('=======> ', newData);
 
     const newObject = JSON.stringify(newData, null, 2);
     fs.writeFile('./db/db.json', newObject, (err, data) => {
-      if (err) throw err;  
-      res.json(data);     
-   });             
-  }); 
-});  
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+});
 
 // SERVER
+const PORT = 3000;
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
